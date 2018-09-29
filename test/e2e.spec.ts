3b6f2f318e21bb1f61a82326dfbceb12a0887570
Sergey Bekrin <sergey@bekrin.me>
2018-09-29 23:17:06 +0200

    chore: update dev dependencies

--- test/e2e.spec.ts
+++ test/e2e.spec.ts
@@ -1,30 +1,29 @@
-import fs from 'fs';
-import path from 'path';
-import puppeteer from 'puppeteer';
-import express from 'express';
-import getPort from 'get-port';
+import { join } from 'path';
+import * as puppeteer from 'puppeteer';
+import * as express from 'express';
+import * as getPort from 'get-port';
 import { Server } from 'http';
 
-let server: Server;
-let browser: puppeteer.Browser;
-let page: puppeteer.Page;
-
-beforeAll(async () => {
-  const port = await getPort();
-  const app = express();
-  app.use(express.static(path.join(__dirname, '__fixtures__')));
-  server = app.listen(port);
-  browser = await puppeteer.launch();
-  page = await browser.newPage();
-  page.goto(`http://localhost:${port}`);
-});
-
-afterAll(async () => {
-  await browser.close();
-  await server.close();
-});
-
 describe('Integration', () => {
+  let server: Server;
+  let browser: puppeteer.Browser;
+  let page: puppeteer.Page;
+
+  beforeAll(async () => {
+    const port = await getPort();
+    const app = express();
+    app.use(express.static(join(__dirname, '__fixtures__')));
+    server = app.listen(port);
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    page.goto(`http://localhost:${port}`);
+  });
+
+  afterAll(async () => {
+    await browser.close();
+    await server.close();
+  });
+
   it('sets class name once font is ready', async () => {
     await page.waitForSelector('.system-font');
     await page.waitForSelector('.roboto900-font');
